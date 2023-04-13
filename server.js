3478c8235734c08c7b4e88c8e9514f8d30558152
Nestor <you@example.com>
2023-04-12 21:40:48 -0400

    used POST method to connect users input to backend

--- server.js
+++ server.js
@@ -30,6 +30,37 @@ app.get('/api/notes', (req, res) => {
   })
 });
 
+app.post("/api/notes", (req, res) => {
+    const currentNote = req.body;
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
+      if (error) {
+          return console.log(error)
+      }
+      notes = JSON.parse(notes)
+      if (notes.length > 0) {
+      let lastId = notes[notes.length - 1].id
+      var id =  parseInt(lastId)+ 1
+      } else {
+        var id = 10;
+      }
+      let newNote = { 
+        title: currentNote.title, 
+        text: currentNote.text, 
+        id: id 
+        }
+      var newNotesArr = notes.concat(newNote)
+      fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNotesArr), (error, data) => {
+        if (error) {
+          return error
+        }
+        console.log(newNotesArr)
+        res.json(newNotesArr);
+      })
+  });
+ 
+});
+
+
    
 
 
