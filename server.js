28428c7813861f12420bec587e2cc33d045de2f6
Nestor <you@example.com>
2023-04-13 16:17:19 -0400

    Pushing Heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 app.get("/notes", (req, res) => {
