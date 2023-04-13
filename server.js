a3aab029ddc4dbed7fb001463515414394bdc433
Nestor <you@example.com>
2023-04-12 23:03:29 -0400

    updated server.js & db.json

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 var PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(__dirname + '/public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
