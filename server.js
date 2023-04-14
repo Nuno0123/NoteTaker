e6da4e9d3c3ec4fd9827ef26baf18d0ff93a011a
Nestor <you@example.com>
2023-04-13 21:34:55 -0400

    moved get * in server.js

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-});
+
 app.get("/notes", (req, res) => {
-   res.sendFile(path.join(__dirname, "./public/notes.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
 app.get("/api/notes", (req, res) => {
@@ -22,6 +20,10 @@ app.get("/api/notes", (req, res) => {
       }
       res.json(JSON.parse(notes))
   })
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
 app.post("/api/notes", (req, res) => {
